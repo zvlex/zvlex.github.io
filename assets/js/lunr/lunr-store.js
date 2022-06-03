2befc7a3e63e3f29f68d33e0c1c0f83ac95747eb
zvlex <zvlex@users.noreply.github.com>
2022-06-03 21:09:40 +0000

    jekyll build from Action 8f505948cf537b948a4dc8c929a053d00d5d04a8

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Ruby for Junior Developers",
+        "excerpt":"Ruby for Junior Developers - is a short introduction course based on my knowledge and experience for people, who wants to learn Ruby programming language from scratch. To begin only a basic knowledge of Unix/Linux is required. In this course, I use Fedora Linux Workstation. You can download it from...","categories": [],
+        "tags": ["Ruby"],
+        "url": "/2022/04/19/ruby-for-junior-developers.html",
+        "teaser": null
+      },{
+        "title": "Installing Ruby with asdf on Fedora Linux",
+        "excerpt":"I’m using Fedora Linux distribution for all examples here, if you are using another system, there may be differences in packages to install. Let’s begin. Open your terminal and run these commands: sudo dnf -y groupinstall \"Development Tools\" sudo dnf install vim git curl wget zsh util-linux-user I prefer zsh...","categories": [],
+        "tags": ["Ruby","Fedora"],
+        "url": "/2022/04/20/install-ruby-with-asdf-on-fedora-linux.html",
+        "teaser": null
+      },{
+        "title": "Ruby Basics",
+        "excerpt":"In this article, we will get acquainted with the basic types of Ruby, consider the syntax and learn how to use them. All code will be entered in the interactive console - irb Ruby.new Create new file terminal.rb with this code: 1 2 3 4 5 6 7 8 9...","categories": [],
+        "tags": ["Ruby"],
+        "url": "/2022/04/29/ruby-basics.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": ["Ruby","Rails","Elixir"],
+        "url": "/jekyll/update/2022/06/02/welcome-to-jekyll.html",
+        "teaser": null
+      }]
